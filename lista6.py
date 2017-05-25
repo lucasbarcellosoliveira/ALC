78e56622ae5e1b0fab2f46349a45a9e9082b2cea
Lucas Barcellos <lucas.barcellos.oliveira@poli.ufrj.br>
2017-05-25 18:50:27 -0300

    Add files via upload

---
+++ lista6.py
@@ -0,0 +1,75 @@
+from math import *
+
+def f1(x, y):
+    return -2*x*(y**2)
+
+def f2(x, y, y_1, m=1.0, c=0.2,k=1, w=0.5):
+    return (2*sin(w*x)+sin(2*w*x)+cos(3*w*x)-k*y-c*y_1)/m
+
+def f3(x, y, y_1, kd=1, g=9.81):
+    return -g-kd*y_1*abs(y_1)
+
+def EulerEDO(h, x=0, y=1, lim_sup=2):
+    #pontos=[[x,y]]
+    print [x,y]
+    while x+h-0.001<=lim_sup:
+        y+=h*f1(x,y)
+        x+=h
+        #pontos+=[[x,y]]
+        print [x,y]
+    #return pontos
+
+def RK2EDO(h, x=0, y=1, lim_sup=2):
+    #pontos=[[x,y]]
+    print [x,y]
+    while x+h-0.001<=lim_sup:
+        K1=f1(x,y)
+        K2=f1(x+h,y+h*K1)
+        y+=(h/2.0)*(K1+K2)
+        x+=h
+        #pontos+=[[x,y]]
+        print [x,y]
+    #return pontos
+
+def RK4EDO(h, x=0, y=1, lim_sup=2):
+    #pontos=[[x,y]]
+    print [x,y]
+    while x+h-0.001<=lim_sup:
+        K1=f1(x,y)
+        K2=f1(x+(h/2),y+(h/2)*K1)
+        K3=f1(x+(h/2),y+(h/2)*K2)
+        K4=f1(x+h,y+h*K3)
+        y+=(h/6.0)*(K1+2*K2+2*K3+K4)
+        x+=h
+        #pontos+=[[x,y]]
+        print [x,y]
+    #return pontos
+
+def TaylorEDO2(h, x=0, y=0, y_1=0, lim_sup=100):
+    #pontos=[[x,y]]
+    print [x,y]
+    while x+h-0.001<=lim_sup:
+        y_2=f2(x,y,y_1) #f3
+        y+=y_1*h+(y_2/2.0)*(h**2)
+        y_1+=y_2*h
+        x+=h
+        #pontos+=[[x,y]]
+        print [x,y]
+    #return pontos
+
+def RKNEDO2(h, x=0, y=0, y_1=0, lim_sup=100):
+    #pontos=[[x,y]]
+    print [x,y]
+    while x+h-0.001<=lim_sup:
+        K1=(h/2.0)*f2(x,y,y_1) #f3
+        Q=(h/2.0)*(y_1+K1/2.0)
+        K2=(h/2.0)*f2(x+h/2.0,y+Q,y_1+K1) #f3
+        K3=(h/2.0)*f2(x+h/2.0,y+Q,y_1+K2) #f3
+        L=h*(y_1+K3)
+        K4=(h/2.0)*f2(x+h,y+L,y_1+2*K3) #f3
+        y+=h*(y_1+(K1+K2+K3)/3.0)
+        y_1+=(K1+2*K2+2*K3+K4)/3.0
+        x+=h
+        #pontos+=[[x,y]]
+        print [x,y]
+    #return pontos
